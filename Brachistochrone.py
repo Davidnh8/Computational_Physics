453983318b7460bf61a180b929ea505eed1e7b23
Davidnh8 <Davidnh8@gmail.com>
2018-11-19 22:38:15 -0700

    brachi

--- Brachistochrone.py
+++ Brachistochrone.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -34,12 +34,48 @@ y2=x2**12
 linestyle='-'
 linewidth=3
 
-fig,ax=plt.subplots(1,figsize=(16,16))
-ax.plot(x,y,color="red",linewidth=linewidth)
-ax.plot(xl,yl,color="black",linewidth=linewidth)
-#ax.plot(xc,yc)
-ax.plot(x2,y2,linewidth=linewidth)
+#fig,axs=plt.subplots(2,2,figsize=(11*1.8,8.5*1.8))
+
+#for ax in axs:
+# ax.plot
+#ax.plot(x+1,y,color="red",linewidth=linewidth)
+#ax.plot(xl+1,yl,color="black",linewidth=linewidth)
+#ax.plot(x2+1,y2,linewidth=linewidth)
+# =============================================================================
+# limir=[[0.5,1],[0,0.5]]
+# limic=[[0,0.5],[0.5,1]]
+# for n,row in enumerate(axs):
+#     for m,col in enumerate(row):
+#         col.plot(x+1,y,color="red",linewidth=linewidth)
+#         col.plot(xl+1,yl,color="black",linewidth=linewidth)
+#         col.plot(x2+1,y2,color="blue",linewidth=linewidth)
+#         col.set_ylim(limir[n])
+#         col.set_xlim(limic[m])
+# 
+# =============================================================================
+limir=[[0.5,1],[0,0.5]]
+limic=[[0,0.5],[0.5,1]]
+for n in range(2):
+    for m in range(2):
+        fig,ax=plt.subplots(1,figsize=[11,8.5])
+        
+        ax.plot(x+1,y,color="red",linewidth=linewidth)
+        ax.plot(xl+1,yl,color="black",linewidth=linewidth)
+        ax.plot(x2+1,y2,color="blue",linewidth=linewidth)
+        ax.set_ylim(limir[n])
+        ax.set_xlim(limic[m])
+        ax.grid()
+        ax.axis('off')
+        fig.tight_layout(pad=-0)
+        fig.savefig("Brachistochrone"+str(n)+str(m))
+
+
+
+
+
+
+
 
 fig.savefig("Brachistochrone", dpi=300)
-
+ax.grid()
 plt.show()
